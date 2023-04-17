7680033ad3bc09b65a8a7b0667c8f418240e145d
rutvax <Rutva@Rutvas-MBP.attlocal.net>
2023-04-16 18:32:17 -0700

    update

---
+++ PyBank/mains.py
@@ -0,0 +1,71 @@
+# Import Dependencies
+import os
+import csv
+
+# File Path
+budget_csv = os.path.join('PyBank','Resources','budget_data.csv')
+
+# Create empty lists for looping
+total_months = []
+total_profit = []
+monthly_profit_change = []
+ 
+# Open csv as read
+with open(budget_csv, 'r') as csv_file:
+
+     #Create csv reader
+    csvreader = csv.reader(csv_file,delimiter=",") 
+
+    # Skip the header labels 
+    header = next(csvreader)  
+
+    for row in csvreader:
+
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
+
+    for x in range(len(total_profit)-1):
+
+        monthly_profit_change.append(total_profit[x+1]-total_profit[x])
+
+    # max and min of profit change per month 
+    max_increase = max(monthly_profit_change)
+    min_increase = min(monthly_profit_change)
+
+    month_max_increase = monthly_profit_change.index(max(monthly_profit_change))+1 # +1 adding one to index to give actual number of month
+    month_min_increase = monthly_profit_change.index(min(monthly_profit_change))+1 # +1 adding one to index to give actual number of month
+
+#Printing Analysis 
+
+    print("Financial Analysis")
+    print("----------------------------")
+    print(f"Total Months {len(total_months)}")
+    print(f"Total: {sum(total_profit)}")
+    print(f"Average Change: ${round(sum(monthly_profit_change) / len(monthly_profit_change), 2)}") #round lets you round to how many decimal places you want (2)
+    print(f"Greatest Increase in Profits: {total_months[month_max_increase]} $({str(max_increase)})")
+    print(f"Greatest Decrease in Profits: {total_months[month_min_increase]} $({str(min_increase)})")
+
+
+
+
+financial_output = os.path.join('PyBank','Analysis','Financial_Analysis_Sum.txt')
+
+with open(financial_output,"w") as file:
+    
+# Write methods to print to Financial_Analysis_Summary 
+# ("\n") lets you skip to the next line 
+    file.write("Financial Analysis")
+    file.write('\n')
+    file.write("----------------------------")
+    file.write('\n')
+    file.write(f"Total Months: {len(total_months)}")
+    file.write('\n')
+    file.write(f"Total: ${sum(total_profit)}")
+    file.write('\n')
+    file.write(f"Average Change: {round(sum(monthly_profit_change) / len(monthly_profit_change),2)}")
+    file.write('\n')
+    file.write(f"Greatest Increase in Profits: {total_months[month_max_increase]} (${(str(max_increase))})")
+    file.write('\n')
+    file.write(f"Greatest Decrease in Profits: {total_months[month_min_increase]} (${(str(min_increase))})")
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,83 @@
+#Import Dependencies
+import os
+import csv
+
+# Set file path
+election_data = os.path.join('PyPoll','Resources','election_data.csv')
+
+#Empty list
+voter_id = []
+county = []
+candidate = []
+runners = ['Charles Casper Stockham', 'Diana DeGette', 'Raymon Anthony Doane']
+vote_count = [0,0,0]
+
+#create read file
+with open(election_data, 'r') as csvfile:
+    csvreader = csv.reader(csvfile,delimiter= ',')
+
+    #Skip over header in csv
+    header = next(csvreader)
+
+    for row in csvreader:
+        voter_id.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
+
+    for runner in candidate:
+        if runner == runners[0]:
+            vote_count[0]= vote_count[0]+1
+        elif runner == runners[1]:
+            vote_count[1] = vote_count[1] + 1
+        elif runner == runners[2]:
+            vote_count[2] = vote_count[2] +1
+
+total_votes = len(voter_id)
+
+
+voting_percent = []
+for count in vote_count:
+    percent = round((count / total_votes) * 100, 3)
+    voting_percent.append(percent)
+
+winner_index = vote_count.index(max(vote_count))
+winner = runners[winner_index]
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+for x in range(len(runners)):
+     print(f"{runners[x]}: {voting_percent[x]}% ({vote_count[x]})")
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+
+election_output = os.path.join('PyPoll', 'Analysis', 'election_data.txt')
+
+with open(election_output, "w") as file:
+
+    file.write('\n')
+    file.write('Election Results')
+    file.write('\n')
+    file.write('----------------------------')
+    file.write('\n')
+    file.write(f'Total Votes: {total_votes}')
+    file.write('\n')
+    file.write('----------------------------')
+    for x in range(len(runners)):
+        file.write('\n')
+        file.write(f"{runners[x]}: {voting_percent[x]}% ({vote_count[x]})")
+    file.write('\n')
+    file.write('----------------------------')
+    file.write('\n')
+    file.write(f'Winner: {winner}')
+    file.write('\n')
+    file.write('----------------------------')
+
+
+
+
+
+
