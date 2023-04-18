1f920725ee7a9caae81a57dd490f5fdddc1f648e
rutvax <Rutva@Rutvas-MBP.attlocal.net>
2023-04-18 12:27:55 -0700

    add

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,21 +5,22 @@ import csv
 # Set file path
 election_data = os.path.join('PyPoll','Resources','election_data.csv')
 
-#Empty list
+#Empty list for looping 
 voter_id = []
 county = []
 candidate = []
 runners = ['Charles Casper Stockham', 'Diana DeGette', 'Raymon Anthony Doane']
-vote_count = [0,0,0]
+vote_count = [0,0,0] #each candidate is starting off with 0 votes 
 
-#create read file
+#open csv as read
 with open(election_data, 'r') as csvfile:
     csvreader = csv.reader(csvfile,delimiter= ',')
 
-    #Skip over header in csv
+    #Skip over header label
     header = next(csvreader)
 
     for row in csvreader:
+        
         voter_id.append(row[0])
         county.append(row[1])
         candidate.append(row[2])
@@ -43,16 +44,19 @@ for count in vote_count:
 winner_index = vote_count.index(max(vote_count))
 winner = runners[winner_index]
 
+#Print results
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
-for x in range(len(runners)):
+for x in range(len(runners)): #how many candidates (3)
      print(f"{runners[x]}: {voting_percent[x]}% ({vote_count[x]})")
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
 
+
+#path for write
 
 election_output = os.path.join('PyPoll', 'Analysis', 'election_data.txt')
 
